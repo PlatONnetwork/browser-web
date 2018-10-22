0540bd1559597e072556502cd66a2908edf28a6e
dongqile <dongqile@juzix.io>
2018-10-22 19:55:12 +0800

    code framework

---
+++ server/src/main/java/com/platon/browserweb/Controller/BlockController.java
@@ -0,0 +1,20 @@
+package com.platon.browserweb.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User: dongqile
+ * Date: 2018/7/10
+ * Time: 22:34
+ */
+@RestController
+    @RequestMapping("/exchange_api")
+public class BlockController extends BasicsController {
+
+    private static Logger logger = LoggerFactory.getLogger(BlockController.class);
+
+
+}
