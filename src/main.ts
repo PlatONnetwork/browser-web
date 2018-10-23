d0b8cc71fca197b493f7fc417c553a34a2486479
liangyanxiang <liangyanxiang@juzix.io>
2018-10-23 18:01:19 +0800

    添加elementUI  vue-clipboard2

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,30 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import VueI18n from 'vue-i18n'// 多语言
+import VueI18n from 'vue-i18n'
 import messages from '@/lang/index'
+import ElementUI from 'element-ui'
+import VueClipboard from 'vue-clipboard2'
 
 // css
 import '../static/css/reset.css'
+import '../static/css/element-ui.css'
 
 // less
 import '@/less/index.less'
 
 Vue.use(VueI18n)
+Vue.use(ElementUI)
+Vue.use(VueClipboard)
 
 Vue.config.productionTip = false
 
 const browserLanguage: string = navigator.language.toLowerCase()
 const i18n = new VueI18n({
-    // 定义默认语言
     locale: browserLanguage,
     messages
 })
 
-/* eslint-disable no-new */
 export default new Vue({
     el: '#app',
     router,
