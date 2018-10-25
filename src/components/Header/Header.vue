4ecc8f562d08b834976677c2071ffe8bce766e76
yeqiuna <yeqiuna@juzix.io>
2018-10-25 18:09:36 +0800

    sj

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,34 +1,59 @@
 <template>
     <div class="header-wrap">
-        <el-menu :default-active="$route.path" :router="true" class="el-menu-demo" mode="horizontal" active-text-color="#FFE32C">
-            <el-menu-item index="/">
-                <router-link to="/">首页</router-link>
-            </el-menu-item>
-            <el-submenu index="2">
-                <template slot="title">区块链</template>
-                <el-menu-item index="/trade">
-                    <router-link to="/trade">查看交易</router-link>
-                </el-menu-item>
-                <el-menu-item index="/trade-pending">
-                    <router-link to="/trade-pending">查看待处理交易</router-link>
-                </el-menu-item>
-                <el-menu-item index="/block">
-                    <router-link to="/block">查看区块</router-link>
-                </el-menu-item>
-            </el-submenu>
-            <el-menu-item index="/node">
-                <router-link to="/node">节点</router-link>
-            </el-menu-item>
-            <el-submenu index="3">
-                <template slot="title">更多</template>
-                <el-menu-item index="/wallet">
-                    <router-link to="/wallet">钱包</router-link>
-                </el-menu-item>
-                <el-menu-item index="/document">
-                    <router-link to="/document">开发帮助</router-link>
-                </el-menu-item>
-            </el-submenu>
-        </el-menu>
+        <div class="header-content">
+            <div class="leftdiv">
+                <a class="logo"><img class="icon" :src='iconSrc'/></a>
+                <el-menu :default-active="$route.path" :router="true" class="el-menu-demo" mode="horizontal" background-color="#242c39" text-color ="#FFF" active-text-color="#FFE32C">
+                    <el-menu-item index="/">
+                        <router-link to="/">首页</router-link>
+                    </el-menu-item>
+                    <el-submenu index="2">
+                        <template slot="title">区块链</template>
+                        <el-menu-item index="/trade">
+                            <router-link to="/trade">查看交易</router-link>
+                        </el-menu-item>
+                        <el-menu-item index="/trade-pending">
+                            <router-link to="/trade-pending">查看待处理交易</router-link>
+                        </el-menu-item>
+                        <el-menu-item index="/block">
+                            <router-link to="/block">查看区块</router-link>
+                        </el-menu-item>
+                    </el-submenu>
+                    <el-menu-item index="/node">
+                        <router-link to="/node">节点</router-link>
+                    </el-menu-item>
+                    <el-submenu index="3">
+                        <template slot="title">更多</template>
+                        <el-menu-item index="/wallet">
+                            <router-link to="/wallet">钱包</router-link>
+                        </el-menu-item>
+                        <el-menu-item index="/document">
+                            <router-link to="/document">开发帮助</router-link>
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </div>
+                <div>
+                    <el-dropdown class="dropdown1">
+                        <span class="el-dropdown-link">
+                            {{Net}}<i class="el-icon-arrow-down el-icon--right"></i>
+                        </span>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item @click.native="changeNet('MainNet(主网络)')">MainNet(主网络)</el-dropdown-item>
+                            <el-dropdown-item @click.native="changeNet('TestNet(测试网络)')">TestNet(测试网络)</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                    <el-dropdown class="dropdown2">
+                        <span class="el-dropdown-link">
+                            {{Language}}<i class="el-icon-arrow-down el-icon--right"></i>
+                        </span>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item @click.native="changeLanguage('简体中文')">简体中文</el-dropdown-item>
+                            <el-dropdown-item @click.native="changeLanguage('English')">English</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </div>
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -38,14 +63,43 @@
         name: 'header-wrap',
         //实例的数据对象
         data () {
-            return {}
+          return {
+              iconSrc: '/static/images/platon.png',
+              Net: 'MainNet',
+              Language: '简体中文'
+            //   options:[
+			// 		{
+			// 			value: 'CN',
+			// 			label: '中文简体'
+			// 		},
+			// 		{
+			// 			value: 'EN',
+			// 			label: 'English'
+			// 		}
+            //     ],
+                }
         },
         //数组或对象，用于接收来自父组件的数据
         props: {},
         //计算
-        computed: {},
+        computed: {
+           
+            },
         //方法
-        methods: {},
+        methods: {
+            changeNet(value){  // 修改网络
+                this.Net = value
+                console.log(value)
+            },
+            changeLanguage(lang){  // 修改语言
+                this.Language = lang
+                console.log(lang)
+            },
+            // changeLanguage(value){  //修改语言
+			// 	let locale = this.$i18n.locale
+			// 	locale === 'zh' ? this.$i18n.locale = 'en' : this.$i18n.locale = 'zh'
+            // },
+        },
         //生命周期函数
         created(){},
         //监视
@@ -55,6 +109,51 @@
     }
 </script>
 <style lang="less" scoped>
+.header-wrap{
+    background-color: #242c39;
+    width: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 60px;
+    .header-content{
+        top: 8%;
+        left: 50%;
+        width: 1000px;
+        margin: 0 auto;
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .logo{
+            display: inline-block;
+            height: 100%;
+        }
+        .leftdiv{
+            height: 100%;
+        }
+    }
+    .leftimg{
+        position: absolute;
+        top: 0;
+        left: 30%;
+        margin: 0 0 0 -450px
+    }
+    .el-menu-demo{
+        display: inline-block;
+    }
+    .header-content el-menu-demo{
+            height: 70px;
+    }
+    .dropdown1{
+        color: #fff;
+    }
+    .dropdown2{
+        color: #fff;
+        width: 80px;
+    }
+    
 
+}
 </style>
 
