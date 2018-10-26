f5f607fcf48ee6acde0b880daf8cc3f6dc9176a6
liangyanxiang <liangyanxiang@juzix.io>
2018-10-26 13:57:51 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
     <div id="app">
-        <com-header></com-header>
-        <div class="com-container">
+        <!-- <com-header></com-header> -->
+        <!-- <div class="com-container"> -->
             <router-view></router-view>
-        </div>
+        <!-- </div> -->
         <!-- <el-row :gutter='20'>
             <el-col :span="18" :offset="3">
 
             </el-col>
         </el-row> -->
-        <com-footer></com-footer>
+        <!-- <com-footer></com-footer> -->
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import comHeader from '@/components/header/header.vue'
-import comFooter from '@/components/footer/footer.vue'
+// import comHeader from '@/components/header/header.vue'
+// import comFooter from '@/components/footer/footer.vue'
 @Component({
     components:{
-        comHeader,
-        comFooter
+        // comHeader,
+        // comFooter
     }
 })
 export default class App extends Vue {
@@ -30,8 +30,5 @@ export default class App extends Vue {
 </script>
 
 <style lang="less" scoped>
-    .main{
-        background:#f0f0f0;
-        height:100%;
-    }
+
 </style>
