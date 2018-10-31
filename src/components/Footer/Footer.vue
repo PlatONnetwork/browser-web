8cbcb905be46e0c5d57cbd2ad89b0df674c13ab2
hufu <hufu@juzix.net>
2018-10-31 16:37:47 +0800

    update footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -28,33 +28,22 @@
                     <a href="http://www.baidu.com" class="icon-link-1" target="_blank"></a>
                 </li>
                 <li>
-                    <a href="http://www.baidu.com" class="icon-link-2" target="_blank">
-                    </a>
+                    <a href="http://www.baidu.com" class="icon-link-2" target="_blank"></a>
                 </li>
                 <li>
-                    <a href="http://www.baidu.com" class="icon-link-3" target="_blank">
-                    <img :src="linkImg3"/>
-                    </a>
+                    <a href="http://www.baidu.com" class="icon-link-3" target="_blank"></a>
                 </li>
                 <li>
-                    <a href="http://www.baidu.com" class="icon-link-4" target="_blank">
-                    <img :src="linkImg4"/>
-                    </a>
+                    <a href="http://www.baidu.com" class="icon-link-4" target="_blank"></a>
                 </li>
                 <li>
-                    <a href="http://www.baidu.com" class="icon-link-5" target="_blank">
-                    <img :src="linkImg5"/>
-                    </a>
+                    <a href="http://www.baidu.com" class="icon-link-5" target="_blank"></a>
                 </li>
                 <li>
-                    <a href="http://www.baidu.com" class="icon-link-6" target="_blank">
-                    <img :src="linkImg6"/>
-                    </a>
+                    <a href="http://www.baidu.com" class="icon-link-6" target="_blank"></a>
                 </li>
                 <li>
-                    <a href="http://www.baidu.com" class="icon-link-7" target="_blank">
-                    <img :src="linkImg7"/>
-                    </a>
+                    <a href="http://www.baidu.com" class="icon-link-7" target="_blank"></a>
                 </li>
             </ul>
         </div>
@@ -105,9 +94,9 @@
         justify-content: space-between;   //弹性布局，从上到下顺序排列子div
         height: 160px; // UI设计图 190px
         color: #FFF;
-        background-color: #040B27;
-        padding: 0 100px;
-        margin: 30px auto 0;
+        background-color: #0D1333;
+        padding: 30px 100px 0 100px;
+        // margin: 30px auto 0;
     }
     .platon{
         background: url('./images/footer-logo.png');
--- src/views/more/wallet.vue
+++ src/views/more/wallet.vue
@@ -1,72 +1,78 @@
 <template>
     <div class="node-wrap">
-        <div class="wallet">
-            <div class="top">
-                <el-breadcrumb separator-class="el-icon-arrow-right">
-                    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                    <el-breadcrumb-item>钱包</el-breadcrumb-item>
-                </el-breadcrumb>
-                <div>
-                    <el-button type="primary">+提交</el-button>
-                </div>
-            </div>
-            <div class="authority-wallet">
-                <h1 class="title">官方钱包</h1>
-                <div class="introduce">
-                    <div class="Edge1">
-                        <img :src="imgSrc"/>
-                        <div class="Edge1-introduce" v-if="walletList.length>0">
-                            <h1>{{walletList[0].title}}</h1>
-                            <p>{{walletList[0].summary}}</p>
-                            <ul class="link">
-                                <li>
-                                    <a href="https:www.baidu.com" target="_blank">{{walletList[0].dowonloadLink}}</a>
-                                </li>
-                                <li>
-                                    <a href="https://t.me/PlatONHK">{{walletList[0].githubLink}}</a>
-                                </li>
-                                <li>
-                                    <a href="https://t.me/PlatONHK" class="icon-link-3">{{walletList[0].developer}}</a>
-                                </li>
-                            </ul>
-                            <!-- <el-button>{{walletList[0].dowonloadLink}}</el-button>  使用ul列表代替el-button
-                            <el-button>{{walletList[0].githubLink}}</el-button>
-                            <el-button>{{walletList[0].developer}}</el-button> -->
-                        </div>
-                    </div>
-                    <div class="Edge">
-                        <img :src="imgSrc"/>
-                        <div class="Edge-introduce"  v-if="walletList.length > 0">
-                            <h1>{{walletList[1].title}}</h1>
-                            <p>{{walletList[1].summary}}</p>
-                            <ul class="link">
-                                <li>
-                                    <a href="https:www.baidu.com" target="_blank">{{walletList[1].dowonloadLink}}</a>
-                                </li>
-                                <li>
-                                    <a href="https://t.me/PlatONHK">{{walletList[1].githubLink}}</a>
-                                </li>
-                                <li>
-                                    <a href="https://t.me/PlatONHK" class="icon-link-3">{{walletList[1].developer}}</a>
-                                </li>
-                            </ul>
-                        </div>
+        <com-header :descriptionProp='descriptionProp'></com-header>
+            <div class="wallet">
+                <p class="watermark">
+                    钱包
+                </p>
+                <div class="top">
+                    <el-breadcrumb separator-class="el-icon-arrow-right">
+                        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                        <el-breadcrumb-item>钱包</el-breadcrumb-item>
+                    </el-breadcrumb>
+                    <div>
+                        <el-button type="primary">+提交</el-button>
                     </div>
                 </div>
-            </div>
-            <div>
+                <div class="authority-wallet">
+                    <h1 class="title">官方钱包</h1>
+                    <div class="introduce">
+                        <div class="Edge1">
+                            <img src="./images/default-logo.png"/>
+                            <div class="Edge1-introduce" v-if="walletList.length>0">
+                                <h1>{{walletList[0].title}}</h1>
+                                <p>{{walletList[0].summary}}</p>
+                                <ul class="link">
+                                    <li>
+                                        <a href="https:www.baidu.com" target="_blank">{{walletList[0].dowonloadLink}}</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://t.me/PlatONHK">{{walletList[0].githubLink}}</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://t.me/PlatONHK" class="icon-link-3">{{walletList[0].developer}}</a>
+                                    </li>
+                                </ul>
+                                <!-- <el-button>{{walletList[0].dowonloadLink}}</el-button>  使用ul列表代替el-button
+                                <el-button>{{walletList[0].githubLink}}</el-button>
+                                <el-button>{{walletList[0].developer}}</el-button> -->
+                            </div>
+                        </div>
+                        <div class="Edge">
+                            <img src="./images/default-logo.png"/>
+                            <div class="Edge-introduce"  v-if="walletList.length > 0">
+                                <h1>{{walletList[1].title}}</h1>
+                                <p>{{walletList[1].summary}}</p>
+                                <ul class="link">
+                                    <li>
+                                        <a href="https:www.baidu.com" target="_blank">{{walletList[1].dowonloadLink}}</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://t.me/PlatONHK">{{walletList[1].githubLink}}</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://t.me/PlatONHK" class="icon-link-3">{{walletList[1].developer}}</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <h1 class="title">第三方钱包</h1>
-                <div>
-                    暂无
+                <div class="third-party-wallet">
+                    <span>没有第三方钱包</span>
+                    <button>Submit</button>
                 </div>
             </div>
-        </div>
+        <com-footer></com-footer>
     </div>
 </template>
 
 <script>
     //import  from ''
     import apiService from '@/services/API-services';
+    import comHeader from '@/components/header/header.vue'
+    import comFooter from '@/components/footer/footer.vue'
     export default {
         //组件名
         name: '',
@@ -74,11 +80,13 @@
         data () {
             return {
                 imgSrc: '/static/images/demo.png',
-                walletList: []
+                walletList: [],
+                descriptionProp:'wallet'
             }
         },
         //数组或对象，用于接收来自父组件的数据
-        props: {},
+        props: {
+        },
         //计算
         computed: {
 
@@ -99,13 +107,25 @@
 
         },
         //组件
-        components: {}
+        components: {
+            comHeader,
+            comFooter
+        }
     }
 </script>
 
 <style lang="less" scoped>
-    .node-wrap{
+    .wallet{
+        padding:60px 100px 0;
         padding-top: 60px;
+        .watermark{
+            opacity: 0.2;
+            font-family: ArialMT;
+            font-size: 64px;
+            color: #3C425D;
+            letter-spacing: 0;
+            line-height: 64px;
+        }
         .top{
             display: flex;
             justify-content: space-between;
@@ -148,7 +168,41 @@
             float: left;
         }
         h1.title{
-            border-bottom: 1px solid rgb(100, 107, 116);
+            font-family: ArialMT;
+            font-size: 12px;
+            color: #D7DDE9;
+            letter-spacing: 0;
+            // border-bottom: 1px solid rgb(100, 107, 116);   原型有下划线，UI去掉
+        }
+        .third-party-wallet{
+            width: 525px;
+            height: 180px;
+            background: url('./images/background-third-party.png') no-repeat;
+            span{
+                display: block;
+                margin: auto;
+                width: 50%;
+                height: 18px;
+                padding: 81px 0 0 0;
+                //UI样式
+                font-family: ArialMT;
+                font-size: 16px;
+                color: #D7DDE9;
+                letter-spacing: 0;
+            }
+            button{
+                width: 80px;
+                height: 32px;
+                margin-left: 200px;
+                background: #252C57;
+                border: 1px solid #333A60;
+                //button中的字体样式
+                font-family: ArialMT;
+                font-size: 12px;
+                color: #FFFF00;
+                letter-spacing: 0;
+                text-align: center;
+            }
         }
     }
 </style>
