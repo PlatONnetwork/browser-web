e0245230f34c7b79e3dbeb5585a7562b1288c02c
liangyanxiang <liangyanxiang@juzix.io>
2018-11-12 11:53:16 +0800

    打包问题

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -69,7 +69,8 @@ const webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[hash].css')
+      filename: utils.assetsPath('css/[name].[hash].css'),
+      allChunks:true
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
