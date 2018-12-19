b23941cc228ee37647a630b336c82834acff1e27
yeqiuna <yeqiuna@juzix.io>
2018-12-19 19:45:42 +0800

    sj

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"http://192.168.9.190:10000"',
-  JSON_ROOT: '"http://192.168.9.190:10000/config"'
+  API_ROOT: '"http://192.168.9.190:10020"',
+  JSON_ROOT: '"http://192.168.9.190:10020/config"'
 })
