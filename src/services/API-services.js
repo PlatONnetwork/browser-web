44f0051d07cc970dd9ef813b1b523db276ffa91c
yeqiuna <yeqiuna@juzix.io>
2019-04-02 11:41:59 +0800

    devv

--- src/services/API-services.js
+++ src/services/API-services.js
@@ -127,11 +127,11 @@ class ApiService {
                     let arr=config.url.split('//');
                     console.log(arr)
                     //本地调试
-                    let arr1=arr[1].split('/');
-                    console.log(arr1)
-                    let url=arr[0]+'//'+arr1[0]+'/'+sessionStorage.getItem('commandContext')+'/'+arr1[1]+'/'+arr1[2];
+                    // let arr1=arr[1].split('/');
+                    // console.log(arr1)
+                    // let url=arr[0]+'//'+arr1[0]+'/'+sessionStorage.getItem('commandContext')+'/'+arr1[1]+'/'+arr1[2];
                     //测试环境
-                    // let url='/'+sessionStorage.getItem('commandContext')+arr[0];
+                    let url='/'+sessionStorage.getItem('commandContext')+arr[0];
                     config.url=url
                     console.log(url)
                     // return config;
