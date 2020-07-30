16f9eccd4402342c6b0808f2066f7fee457e0b9b
QianaWei <weiqianqian@juzix.net>
2020-07-30 16:15:10 +0800

    UC兼容hack

--- src/views/node/node-list.vue
+++ src/views/node/node-list.vue
@@ -8,131 +8,117 @@
           <Item :label="$t('nodeInfo.totalStakePower')">
             <p class="Gilroy-Medium">
               {{ValidatorStatisticData.stakingDelegationValue | unit}}
-              <span class="fontSize13 Gilroy-Regular">LAT</span>
+              <span class="fontSize13 Gilroy-Regular" style="padding-right:1px">LAT</span>
             </p>
           </Item>
           <Item :label="$t('nodeInfo.totalDelegations')">
             <!-- <p>{{(ValidatorStatisticData.stakingDelegationValue-ValidatorStatisticData.stakingValue) | formatMoney}}<span class="fontSize13">LAT</span></p> -->
             <p>
-              <span
-                class="Gilroy-Medium"
-              >{{ ValidatorStatisticData.delegationValue | formatMoney |sliceFloat(0)}}</span>
-              <span
-                style="font-size: 13px;"
-              >{{ ValidatorStatisticData.delegationValue | formatMoney |sliceFloat(1)}}</span>
+              <span class="Gilroy-Medium">{{ ValidatorStatisticData.delegationValue | formatMoney |sliceFloat(0)}}</span>
+              <span style="font-size: 13px;">{{ ValidatorStatisticData.delegationValue | formatMoney |sliceFloat(1)}}</span>
               <span class="fontSize13">&nbsp;LAT</span>
             </p>
           </Item>
-        <Item :label="$t('nodeInfo.stakeRate')">
-          <p>
-            <span
-              class="Gilroy-Medium"
-            >{{ValidatorStatisticData.stakingDelegationValue | percentage(ValidatorStatisticData.issueValue)}}</span>
-            <span class="fontSize13">%</span>
-          </p>
-        </Item>
-      </List>
-      <List class="statistic-info">
-        <h3 class="Gilroy-Medium">{{$t('nodeInfo.currentPeriodReward')}}</h3>
-        <Item :label="$t('blockAbout.blockReward')">
-          <!-- <p>
+          <Item :label="$t('nodeInfo.stakeRate')">
+            <p>
+              <span class="Gilroy-Medium">{{ValidatorStatisticData.stakingDelegationValue | percentage(ValidatorStatisticData.issueValue)}}</span>
+              <span class="fontSize13">%</span>
+            </p>
+          </Item>
+        </List>
+        <List class="statistic-info">
+          <h3 class="Gilroy-Medium">{{$t('nodeInfo.currentPeriodReward')}}</h3>
+          <Item :label="$t('blockAbout.blockReward')">
+            <!-- <p>
             {{ValidatorStatisticData.blockReward | formatMoney}}
             <span class="fontSize13">LAT</span>
           </p>-->
-          <p>
-            <span
-              class="Gilroy-Medium"
-            >{{ValidatorStatisticData.blockReward | formatMoney |sliceFloat(0)}}</span>
-            <span
-              style="font-size: 13px;"
-            >{{ValidatorStatisticData.blockReward | formatMoney |sliceFloat(1)}}</span>
-            <span class="fontSize13">&nbsp;LAT</span>
-          </p>
-        </Item>
-        <Item :label="$t('nodeInfo.stakingReward')">
-          <!-- <p>
+            <p>
+              <span class="Gilroy-Medium">{{ValidatorStatisticData.blockReward | formatMoney |sliceFloat(0)}}</span>
+              <span style="font-size: 13px;">{{ValidatorStatisticData.blockReward | formatMoney |sliceFloat(1)}}</span>
+              <span class="fontSize13">&nbsp;LAT</span>
+            </p>
+          </Item>
+          <Item :label="$t('nodeInfo.stakingReward')">
+            <!-- <p>
             {{ValidatorStatisticData.stakingReward | formatMoney}}
             <span class="fontSize13">LAT</span>
           </p>-->
-          <p>
-            <span
-              class="Gilroy-Medium"
-            >{{ValidatorStatisticData.stakingReward | formatMoney |sliceFloat(0)}}</span>
-            <span
-              style="font-size: 13px;"
-            >{{ValidatorStatisticData.stakingReward | formatMoney |sliceFloat(1)}}</span>
-            <span class="fontSize13">&nbsp;LAT</span>
-          </p>
-        </Item>
-        <Item :label="$t('nodeInfo.nextRewardAdjustment')">
-          <div class="next-reward-adjustment">
-            <span :style="{width:getPercentage+'%'}"></span>
-            <span></span>
-            <p>{{$t('tradeAbout.block')}}&nbsp;{{getPercentage}}% of {{ValidatorStatisticData.addIssueEnd-ValidatorStatisticData.addIssueBegin}}</p>
-          </div>
-        </Item>
-      </List>
-      <div class="next-epoch">
-        <h3 class="Gilroy-Medium">{{$t('nodeInfo.nextEpoch')}}</h3>
-        <p>{{$t('nodeInfo.updateEpoch')}}</p>
-        <ul>
-          <li v-for="(item,index) in nextSetting" :key="index">{{item}}</li>
-        </ul>
-      </div>
+            <p>
+              <span class="Gilroy-Medium">{{ValidatorStatisticData.stakingReward | formatMoney |sliceFloat(0)}}</span>
+              <span style="font-size: 13px;">{{ValidatorStatisticData.stakingReward | formatMoney |sliceFloat(1)}}</span>
+              <span class="fontSize13">&nbsp;LAT</span>
+            </p>
+          </Item>
+          <Item :label="$t('nodeInfo.nextRewardAdjustment')">
+            <div class="next-reward-adjustment">
+              <span :style="{width:getPercentage+'%'}"></span>
+              <span></span>
+              <p>{{$t('tradeAbout.block')}}&nbsp;{{getPercentage}}% of {{ValidatorStatisticData.addIssueEnd-ValidatorStatisticData.addIssueBegin}}</p>
+            </div>
+          </Item>
+        </List>
+        <div class="next-epoch">
+          <h3 class="Gilroy-Medium">{{$t('nodeInfo.nextEpoch')}}</h3>
+          <p>{{$t('nodeInfo.updateEpoch')}}</p>
+          <ul>
+            <li v-for="(item,index) in nextSetting" :key="index">{{item}}</li>
+          </ul>
+        </div>
       </div>
     </div>
     <Validator></Validator>
   </div>
 </template>
 <script>
-import apiService from "@/services/API-services";
-import IndexService from "@/services/index-service";
+import apiService from "@/services/API-services"
+import IndexService from "@/services/index-service"
 
-import { mapState, mapActions, mapGetters, mapMutations } from "vuex";
+import { mapState, mapActions, mapGetters, mapMutations } from "vuex"
 
-import List from "@/components/list/list";
-import Item from "@/components/list/item";
-import Validator from "./list";
+import List from "@/components/list/list"
+import Item from "@/components/list/item"
+import Validator from "./list"
 
-let indexService = null;
+let indexService = null
 export default {
   name: "node-list",
-  data() {
-    return {};
+  data () {
+    return {}
   },
   props: {},
   computed: {
     ...mapGetters(["ValidatorStatisticData"]),
-    getPercentage() {
+    getPercentage () {
       if (this.ValidatorStatisticData.addIssueEnd) {
-        const x = 
+        const x =
           (
-            (this.ValidatorStatisticData.currentNumber-this.ValidatorStatisticData.addIssueBegin)/
-            (this.ValidatorStatisticData.addIssueEnd-this.ValidatorStatisticData.addIssueBegin)
-            ) * 100;
+            (this.ValidatorStatisticData.currentNumber - this.ValidatorStatisticData.addIssueBegin) /
+            (this.ValidatorStatisticData.addIssueEnd - this.ValidatorStatisticData.addIssueBegin)
+          ) * 100
         if (x % 1 == 0) {
-          return x;
+          return x
         }
-        return x.toFixed(2);
+        return x.toFixed(2)
       }
-      return 0;
+      return 0
     },
-    nextSetting() {
-      let next = this.ValidatorStatisticData.nextSetting;
+    nextSetting () {
+      let next = this.ValidatorStatisticData.nextSetting
       if (next) {
         if (next < 10) {
-          return "00000" + next;
+          return "00000" + next
         } else if (next < 100) {
-          return "0000" + next;
+          return "0000" + next
         } else if (next < 1000) {
-          return "000" + next;
+          return "000" + next
         } else if (next < 10000) {
-          return "00" + next;
+          return "00" + next
         } else if (next < 100000) {
-          return "0" + next;
+          return "0" + next
         }
       } else {
-        return "000000";
+        return "000000"
       }
     }
   },
@@ -146,34 +132,34 @@ export default {
     ...mapMutations({
       updateValidatorStatisticData: "UPDATE_VALIDATOR_STATIC_DADA",
     }),
-    getNodeStatistic() {
-      let param = {};
+    getNodeStatistic () {
+      let param = {}
       apiService.node
         .statistic(param)
         .then(res => {
-          let { errMsg, code, data } = res;
-          if(code==0){
-            this.updateValidatorStatisticData(data);
-          }else{
-            this.$message.error(errMsg);
+          let { errMsg, code, data } = res
+          if (code == 0) {
+            this.updateValidatorStatisticData(data)
+          } else {
+            this.$message.error(errMsg)
           }
         })
         .catch(error => {
-          this.$message.error(error);
-        });
+          this.$message.error(error)
+        })
     },
   },
   //生命周期函数
-  created() {
-    this.getNodeStatistic();
-    indexService = new IndexService();
-    indexService.getValidatorStatisticData();
+  created () {
+    this.getNodeStatistic()
+    indexService = new IndexService()
+    indexService.getValidatorStatisticData()
 
   },
-  mounted() {
+  mounted () {
   },
-  destroyed() {
-    indexService.disconnect();
+  destroyed () {
+    indexService.disconnect()
   }
 };
 </script>
